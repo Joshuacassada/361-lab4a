563ce332a44d985aa68ee71b1fb4119aa36d02a4
ThomasC02 <thomascantrell02@gmail.com>
2024-09-24 16:11:08 -0400

    final

--- go.c
+++ go.c
@@ -21,9 +21,32 @@ int main() {
     if (left_kid == 0){
         close(reqPipe[0]);
         close(rplyPipe[1]);
+        char req_fd[10], rply_fd[10];
+        snprintf(req_fd, sizeof(req_fd), "%d", reqPipe[1]);
+        snprintf(rply_fd, sizeof(rply_fd), "%d", rplyPipe[0]);
 
-        
-
+        execlp("./user", "Jim", req_fd, rply_fd, NULL);
+        perror('Failed');
+        exit(1);
     }
+
+    pid_t right_kid = fork();
+    if (right_kid < 0){
+        perror("Fork Failed");
+        exit(-1);
+    }
+
+    if (right_kid == 0){
+        close(reqPipe[0]);
+        close(rplyPipe[1]);
+        char req_fd[10], rply_fd[10];
+        snprintf(req_fd, sizeof(req_fd), "%d", reqPipe[1]);
+        snprintf(rply_fd, sizeof(rply_fd), "%d", rplyPipe[0]);
+
+        execlp("./calculator", "Jim", req_fd, rply_fd, NULL);
+        perror('Failed');
+        exit(1);
+    }
+
 
 }
